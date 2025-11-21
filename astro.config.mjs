cf7e0b07d154ba719cb5467ace3fa0f165446d2c
BrooksGeospatial <mac@brooksgeospatial.com>
2025-11-20 20:53:07 -0600

    New Professional Site Launch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Your actual domain name
+  site: 'https://brooksgeospatial.com',
+  
+  // Load the design and 3D engines
+  integrations: [tailwind(), react()],
+});
